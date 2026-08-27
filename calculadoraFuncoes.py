b95af4ad2f577664dff018e44b71905fdc6ee4dd
VictorLCM1227 <victor.luan.costa.matos@gmail.com>
2026-08-27 13:51:00 -0300

    adicionando poo

--- calculadoraFuncoes.py
+++ calculadoraFuncoes.py
@@ -1,9 +1,54 @@
 # CalculadoraFunções
 
+class HistoricoOperacoes:
+    def __init__(self):
+        self.historico = []
+
+    def adicionar(self, operacao, valor):
+        self.historico.append({"operacao": operacao, "valor": valor})
+
+    def mostrar(self):
+        for item in self.historico:
+            print(f"Operação: {item['operacao']} | Valor: {item['valor']}")
+
+    def limpar(self):
+        
+
+
+
+
+
+
+
+
 def somar(numero1, numero2):
     soma = numero1 + numero2
     return soma
 
 def subtrair(numero1, numero2):
-    subtracao = numero1 + numero2
+    subtracao = numero1 - numero2
     return subtracao
+
+def multiplicar(numero1, numero2):
+    produto = numero1 * numero2
+    return produto
+
+def dividir(numero1, numero2):
+    resultado = numero1 / numero2
+    return resultado
+
+def dividirInteiro(numero1, numero2):
+    resultado = numero1 % numero2
+    return resultado
+
+def potenciar(numero1, numero2):
+    resultado = numero1 ** numero2
+    return resultado
+
+def radiciar(numero1, numero2):
+    resultado = numero1 ** (1 / numero2)
+    return resultado
+
+def porcentagem(numero1, numero2):
+    resultado = numero1 * (numero2 / 100)
+    return resultado
--- calculadoraMenu.py
+++ calculadoraMenu.py
@@ -1,26 +1,9 @@
 # calculadoraMenu
 
 '''
-1. objetivo completo:
+objetivo completo:
 Uma aplicação desktop que permite realizar operações matemáticas básicas através de uma interface gráfica.
 
-2. funcionalidades:
-somar
-multiplicar
-subtrair
-dividir
-divisao inteira
-limpar o resultado
-histórico salvo em txt
-porcentagem
-raiz
-potência
-limpar historico
-
-Etapa 1 — Estrutura
-Criar a pasta do projeto
-Criar os módulos
-Definir as dependências
 
 Etapa 2 — Lógica
 Criar função de soma
@@ -47,19 +30,53 @@ Corrigir bugs
 
 import calculadoraUtilidades
 import calculadoraFuncoes
+sistema = calculadoraFuncoes.HistoricoOperacoes()
 
-menu_escolha = calculadoraUtilidades.menu('CALCULADORA EM PYTHON', ['SAIR', 'SOMA', 'SUBTRAÇÃO', 'MULTIPLICAÇÃO', 'DIVISÃO', 'DIVISÃO INTEIRA', 'POTÊNCIA', 'RAIZ', 'PORCENTAGAM', 'VER HISTÓRICO', 'LIMPAR HISTÓRICO'])
+while True:
 
-if menu_escolha == 0:
-    calculadoraUtilidades.cabecalho('SAINDO...')
+    menu_escolha = calculadoraUtilidades.menu('CALCULADORA EM PYTHON', ['SAIR', 'SOMA', 'SUBTRAÇÃO', 'MULTIPLICAÇÃO', 'DIVISÃO', 'DIVISÃO INTEIRA', 'POTÊNCIA', 'RAIZ', 'PORCENTAGEM', 'VER HISTÓRICO', 'LIMPAR HISTÓRICO'])
 
-elif menu_escolha == 1:
-    calculadoraUtilidades.cabecalho('SOMA')
-    print(f'A soma deu: {calculadoraFuncoes.somar(calculadoraUtilidades.leiaFloat('Digite um número: '), calculadoraUtilidades.leiaFloat('Digite outro número para somar: ')):.3f}')
+    if menu_escolha == 0:
+        calculadoraUtilidades.cabecalho('SAINDO...')
+        break
 
-elif menu_escolha == 2:
-    calculadoraUtilidades.cabecalho('SUBTRAÇÃO')
-    print(f'A subtração deu: {calculadoraFuncoes.subtrair(calculadoraUtilidades.leiaFloat('Digite um número: '), calculadoraUtilidades.leiaFloat('Digite outro número para subtrair: ')):.3f}')
+    elif menu_escolha == 1:
+        calculadoraUtilidades.cabecalho('SOMA')
+        print(f'A soma deu: {calculadoraFuncoes.somar(calculadoraUtilidades.leiaFloat('Digite um número: '), calculadoraUtilidades.leiaFloat('Digite outro número para somar: ')):.3f}')
+
+    elif menu_escolha == 2:
+        calculadoraUtilidades.cabecalho('SUBTRAÇÃO')
+        print(f'A subtração deu: {calculadoraFuncoes.subtrair(calculadoraUtilidades.leiaFloat('Digite um número: '), calculadoraUtilidades.leiaFloat('Digite outro número para subtrair: ')):.3f}')
+
+    elif menu_escolha == 3:
+        calculadoraUtilidades.cabecalho('MULTIPLICAÇÃO')
+        print(f'A Multiplicação deu: {calculadoraFuncoes.multiplicar(calculadoraUtilidades.leiaFloat('Digite um número: '), calculadoraUtilidades.leiaFloat('Digite outro número para multiplicar: ')):.3f}')
+
+    elif menu_escolha == 4:
+        calculadoraUtilidades.cabecalho('DIVISÃO')
+        print(f'A divisão deu: {calculadoraFuncoes.dividir(calculadoraUtilidades.leiaFloat('Digite um número: '), calculadoraUtilidades.leiaFloat('Digite outro número para dividir o anterior: '))}')
+
+    elif menu_escolha == 5:
+        calculadoraUtilidades.cabecalho('DIVISÃO INTEIRA')
+        print(f'A divisão inteira deu: {calculadoraFuncoes.dividir(calculadoraUtilidades.leiaFloat('Digite um número: '), calculadoraUtilidades.leiaFloat('Digite outro número para dividir o anterior: '))}')
+
+    elif menu_escolha == 6:
+        calculadoraUtilidades.cabecalho('POTÊNCIA')
+        print(f'A potência deu: {calculadoraFuncoes.potenciar(calculadoraUtilidades.leiaFloat('Digite um número: '), calculadoraUtilidades.leiaFloat('Digite o valor do expoente: '))}')
+
+    elif menu_escolha == 7:
+        calculadoraUtilidades.cabecalho('RAIZ')
+        print(f'A raiz deu: {calculadoraFuncoes.radiciar(calculadoraUtilidades.leiaFloatPositivo('Digite um número: '), calculadoraUtilidades.leiaFloatPositivo('Digite um valor para a raiz: '))}')
+
+    elif menu_escolha == 8:
+        calculadoraUtilidades.cabecalho('PORCENTAGEM')
+        print(f'O resultado é: {calculadoraFuncoes.porcentagem(calculadoraUtilidades.leiaFloat('Digite um número: '), calculadoraUtilidades.leiaFloatPositivo('Digite o valor da porcentagem: '))}')
+
+    elif menu_escolha == 9:
+        calculadoraUtilidades.cabecalho('VER HISTÓRICO')
+
+    elif menu_escolha == 10:
+        calculadoraUtilidades.cabecalho('LIMPAR HISTÓRICO')
 
     
 # repetir a lógica para as outras operações
