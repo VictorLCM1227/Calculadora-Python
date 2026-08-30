239d65d63bce95bb143e954b807a57636e56edca
VictorLCM1227 <victor.luan.costa.matos@gmail.com>
2026-08-30 17:36:56 -0300

    Update Interface.py

---
+++ Interface.py
@@ -0,0 +1,30 @@
+'''
+┌─────────────────────────┐
+│                   123   │
+├─────┬─────┬─────┬───────┤
+│  7  │  8  │  9  │   ÷   │
+├─────┼─────┼─────┼───────┤
+│  4  │  5  │  6  │   ×   │
+├─────┼─────┼─────┼───────┤
+│  1  │  2  │  3  │   −   │
+├─────┼─────┼─────┼───────┤
+│  0  │  C  │  =  │   +   │
+└─────┴─────┴─────┴───────┘
+'''
+
+#criar a janela principal
+
+import tkinter as tk
+from tkinter import ttk, messagebox
+
+janela = tk.Tk()
+janela.title('CALCULADORA')
+janela.geometry('350x500+20+20')
+
+valor_display = 0
+txt_display = ttk.Entry(font=("Arial", 16, "bold"), justify='right', state='disabled', width=)
+
+
+
+
+janela.mainloop()
